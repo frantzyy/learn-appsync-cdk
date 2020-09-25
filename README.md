ae64e5ccfc1f664e4aedf0f54800c5879e8ee166
CHRISTOPHER FRANTZ <christopher.frantz@libertymutual.com>
2020-09-25 15:22:22 -0400

    initial commit working queries in aws console

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This is a blank project for TypeScript development with CDK.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
+Created following this tutorial: https://aws.amazon.com/blogs/mobile/building-scalable-graphql-apis-on-aws-with-cdk-and-aws-appsync/
+
 ## Useful commands
 
  * `npm run build`   compile typescript to js
@@ -12,3 +14,12 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+
+## App Root
+
+./lib/learnappsync-cdk-app-stack.ts
+
+This is the root of the CDK app and where we will be writing the code for our app.
+
+ 
---
+++ lambda-fns/appsync-ds-main.ts
@@ -0,0 +1,36 @@
+import createNote from './createNote';
+import deleteNote from './deleteNote';
+import getNoteById from './getNoteById';
+import listNotes from './listNotes';
+import updateNote from './updateNote';
+
+// import Note = require('./Note'); //TODO: why require and not from?
+
+
+// type AppSyncLambdaEvent = APIGatewayEvent & {
+//     info: {
+//       fieldName: string
+//     },
+//     arguments: {
+//       noteId: string,
+//       note: Note
+//     }
+//   }
+  
+
+exports.handler = async (event:any) => {
+    switch (event.info.fieldName) {
+        case "getNoteById":
+            return await getNoteById(event.arguments.noteId);
+        case "createNote":
+            return await createNote(event.arguments.note);
+        case "listNotes":
+            return await listNotes();
+        case "deleteNote":
+            return await deleteNote(event.arguments.noteId);
+        case "updateNote":
+            return await updateNote(event.arguments.note);
+        default:
+            return null;
+    }
+}
